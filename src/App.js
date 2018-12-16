6c023e0af67b3403bdc861a83161aa7498f066a8
RajaRaviVarma <ravi.varma@cybrilla.com>
2018-12-16 20:46:28 +0530

    Change the application

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import LineChart from './Chart.js'
+import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <select>
+          <option value="bar_chart">Bar Chart</option>
+          <option value="line_chart">Line Chart</option>
+        </select>
+        <LineChart />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
